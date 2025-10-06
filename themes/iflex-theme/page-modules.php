63083c978a8de2bb60d886f2699973af24d1ad12
= <=>
2025-10-06 19:33:36 +0800

    added modules page

---
+++ themes/iflex-theme/page-modules.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<section id="modules-section" class="vh-100 d-flex  bg-danger">
+  <div id="modules-container" class="justify-content-center container text-success d-flex align-items-center h-100 w-100">
+   <h1>Hello this is from a wordpress modules section</h1>
+  </div>
+</section>
+<?php get_footer(); ?>
