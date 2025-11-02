6be621329a9791b5d18178282d7be8258fa71899
= <=>
2025-11-03 02:05:59 +0800

    modified / changed icons on front-page.php

--- themes/iflex-theme/template-parts/floating-info.php
+++ themes/iflex-theme/template-parts/floating-info.php
@@ -2,30 +2,30 @@
   <div class="row  align-items-center  position-relative g-3">
   <!-- first icon-text -->
     <div class="col-6 col-lg-3 justify-content-center   align-items-center  d-flex flex-column">
-        <img src="<?php echo get_theme_file_uri( '/assets/icons/boxing-icon.png' ) ?>" 
+        <img src="<?php echo get_theme_file_uri( '/assets/icons/boxing-icon.webp' ) ?>" 
           width="65" height="65"
           alt="i.Flex Fitness Boxing Program" class="floating-info-icon">
-        <p>BOXING</p>
+        <p class="fw-semibold text-danger">BOXING</p>
     </div>
     <!-- second icon-text -->
      <div class="col-6 col-lg-3 justify-content-center align-items-center  d-flex flex-column">
-        <img src="<?php echo get_theme_file_uri( '/assets/icons/muy-thai-icon.png' ) ?>" 
+        <img src="<?php echo get_theme_file_uri( '/assets/icons/muay-thai.webp' ) ?>" 
           width="65" height="65"
           alt="i.Flex Fitness Muy-Thai Program" class="floating-info-icon">
-        <p>MUY-THAI</p>
+        <p class="fw-semibold text-success">MUAY-THAI</p>
     </div>
     <!-- third icon-text -->
      <div class="col-6 col-lg-3 justify-content-center align-items-center  d-flex flex-column">
-        <img src="<?php echo get_theme_file_uri( '/assets/icons/work-out-icon.png' ) ?>" 
+        <img src="<?php echo get_theme_file_uri( '/assets/icons/work-out.webp' ) ?>" 
           width="65" height="65"
           alt="i.Flex Fitness Work-Out Program" class="floating-info-icon">
-        <p>WORKOUT</p>
+        <p class="fw-semibold text-black">WORKOUT</p>
     </div>  
     <!-- fourth icon-text -->
      <div class="col-6 col-lg-3 justify-content-center align-items-center   d-flex flex-column">
-        <img src="<?php echo get_theme_file_uri( '/assets/icons/ji-jutso-icon.png' ) ?>" 
+        <img src="<?php echo get_theme_file_uri( '/assets/icons/wrestling.webp' ) ?>" 
           width="65" height="65"
-          alt="i.Flex Fitness Ji-Jutso Program" class="floating-info-icon">
-        <p>ji-jutso</p>
+          alt="i.Flex Fitness Jiu-Jitso Program" class="floating-info-icon">
+        <p class="fw-semibold text-danger">JIU-JITSU</p>
     </div>
 </div>
