34596f49c64526bcf2c6a0482924aa8e316c28a5
devjas <devop.jas@gmail.com>
2025-11-10 18:07:03 -0800

    added

--- themes/iflex-theme/front-page.php
+++ themes/iflex-theme/front-page.php
@@ -57,7 +57,7 @@
   <!-- ==========================
        ABOUT US SECTION ||
   =========================== -->
-  <div class="about-us-container py-5 mt-n1 pt-md-3 pt-0 pt-lg-0 w-100 position-relative">
+  <div class="about-us-container container-lg-fluid  mt-n1 pt-md-3 pt-0 pt-lg-0 w-100 position-relative">
     <?php get_template_part( 'template-parts/about', 'us' ); ?>
   </div>
   
