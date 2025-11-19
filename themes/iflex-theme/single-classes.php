ead79e8323bc9c50d40bcb631099bcd8dbec06d1
devjas <devop.jas@gmail.com>
2025-11-19 03:58:16 -0800

    modified layout

--- themes/iflex-theme/single-classes.php
+++ themes/iflex-theme/single-classes.php
@@ -10,16 +10,16 @@
   </section>
   <section class="w-100 d-flex flex-lg-row gap-2 flex-column">
      <div class="single-class-content py-5 py-lg-0 d-flex border w-100 container  justify-content-end align-items-center">
-        <div class=" single-class-cards  d-flex flex-column ">
-            <div class=" d-flex flex-wrap h-100  position-relative">
-              <?php 
+        <div class=" single-class-cards container ">
+              <div class="row row-cols-1 row-cols-md-2">
+                <?php 
                
                for( $i = 1; $i <= 4; $i++){ ?>
                  <div class="box text-light d-flex p-3 flex-column <?php if($i <=2 ){
                     echo 'justify-content-end';
                  } else {
                   echo 'justify-content-start';
-                 } ?>  align-items-center w-50 h-50">
+                 } ?>  align-items-center">
                    <div class="d-flex flex-row justify-content-start align-items-center gap-2 w-100">
                      <span class="dashicons dashicons-yes-alt text-white"></span>
                   <h4  class='text-start fs-4 fw-bold text-dark fs-5  w-100 semibold'>
