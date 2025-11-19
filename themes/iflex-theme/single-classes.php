a468daf7b1a40c1f6f4d05b70dc8962ce6d67dab
devjas <devop.jas@gmail.com>
2025-11-18 23:16:29 -0800

    configured stuff

--- themes/iflex-theme/single-classes.php
+++ themes/iflex-theme/single-classes.php
@@ -9,7 +9,7 @@
     </button>
   </section>
   <section class="w-100 d-flex flex-md-row gap-2 flex-column">
-     <div class="single-class-content d-flex border container  justify-content-end align-items-center">
+     <div class="single-class-content py-5 py-lg-0 d-flex border w-100 container  justify-content-end align-items-center">
         <div class=" single-class-cards  d-flex flex-column ">
             <div class=" d-flex flex-wrap h-100  position-relative">
               <?php 
@@ -20,12 +20,15 @@
                  } else {
                   echo 'justify-content-start';
                  } ?>  align-items-center w-50 h-50">
+                   <div class="d-flex flex-row justify-content-start align-items-center gap-2 w-100">
+                     <span class="dashicons dashicons-yes-alt text-white"></span>
                   <h4  class='text-start fs-4 fw-bold text-dark fs-5  w-100 semibold'>
                    <?php $title = get_field('why_'.$i.'_title'); 
                     if($title){
                       echo esc_html($title);
                     }
                    ?></h4>
+                   </div>
                    
                   <p class='text-start fs-5 w-100'>
                    <?php $content = get_field('why_'.$i.'_content');
