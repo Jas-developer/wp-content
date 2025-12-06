ee232cbcd342b14c7f48d812212a41d445c0a480
Devjas <devop.jas@gmail.com>
2025-12-06 10:25:27 +0800

    modified test

--- themes/iflex-theme/front-page.php
+++ themes/iflex-theme/front-page.php
@@ -16,7 +16,7 @@
     <div class="position-relative mt-5 px-3 px-md-0">
       <h1 id="hero-section-heading" class="position-relative  z-2"><span class="text-white">i.</span>Flex <span class="text-white">Fitness</span></h1>
       <div class="w-100 d-flex gap-2 flex-column">
-         <h3 class="fw-semibold text-white z-2">Training Ground for Greatness!Test</h3>
+         <h3 class="fw-semibold text-white z-2">Training Ground for Greatness!</h3>
         <a class="btn btn-danger z-2 px-5 border border-0 text-light fw-bold  w-auto align-self-start">
          <span>JOIN NOW</span> <span class="dashicons dashicons-arrow-right-alt2"></span>
         </a>
